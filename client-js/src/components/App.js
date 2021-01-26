bf0a375ab62a8002d0128c0e665e73cef9d2a8a5
Joseph Heidari <agile.8118@usa.com>
2021-01-26 16:15:53 +0330

    Removes the pagination logic in front-end

--- client-js/src/components/App.js
+++ client-js/src/components/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from "react";
 import axios from "axios";
 import Articles from "./Articles";
-import Pagination from "./Pagination";
 
 class App extends Component {
   state = {
     articles: null,
-    pagination: null,
   };
 
   componentDidMount() {
@@ -19,7 +17,6 @@ class App extends Component {
     );
 
     this.setState({
-      pagination: data.paginationData,
       articles: data.articlesData,
     });
   }
@@ -28,23 +25,6 @@ class App extends Component {
     return (
       <React.Fragment>
         {this.state.articles && <Articles articles={this.state.articles} />}
-        {this.state.pagination && (
-          <Pagination
-            showingUntil={this.state.pagination.showingUntil}
-            showingFrom={this.state.pagination.showingFrom}
-            totalResults={this.state.pagination.totalResults}
-            totalPagesNum={this.state.pagination.totalPages}
-            currentPage={this.state.pagination.currentPage}
-            onButtonClick={(page) => {
-              window.scroll({
-                top: 0,
-                behavior: "smooth",
-              });
-
-              this.fetch(page);
-            }}
-          />
-        )}
       </React.Fragment>
     );
   }
--- server/routes/articleRoutes.js
+++ server/routes/articleRoutes.js
@@ -2,7 +2,18 @@ const { DB } = require("../database");
 
 module.exports = (app) => {
   app.get("/api/articles", async (req, res) => {
-    const articles = await DB.find("articles", {});
+    const page = parseInt(req.query.page);
+    const perPage = 12;
+
+    const articles = await DB.find(
+      "articles",
+      {},
+      {
+        limit: perPage,
+        skip: perPage * (page - 1),
+        sort: { date: -1 },
+      }
+    );
 
     res.send({
       articlesData: articles,
